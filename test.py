1e67300cd9411cdbbc73875f46b16148f20185b3
Yukari <mistelinn@gmail.com>
2020-06-15 09:20:48 +0300

    Implement #3

--- test.py
+++ test.py
@@ -9,9 +9,32 @@ from model import SAN
 from utils import ImageSplitter
 
 
+def process(model, splitter, batch_size, input_fname, output_fname):
+    img = Image.open(input_fname).convert('RGB')
+    patches = splitter.split(img)
+
+    if batch_size > 1:
+        b = batch_size
+        l = len(patches)
+        patches = [torch.cat(patches[i:min(i+b, l)]) for i in range(0, l, b)]
+
+    start_time = time.time()
+    with torch.no_grad():
+        out = [model(p.to(device)) for p in patches]
+    print("Done in %.3f seconds!" % (time.time() - start_time))
+
+    if batch_size > 1:
+        out = [torch.split(pb, 1, dim=0) for pb in out]
+        out = [p for tup in out for p in tup]
+
+    out_img = splitter.merge(out)
+    out_img.save(output_fname)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--scale', type=int, default=2, help="scale")
+    parser.add_argument('--batch_size', type=int, default=1, help="Batch size")
     parser.add_argument('--checkpoint', type=str,
                         help='The filename of pickle checkpoint.')
     parser.add_argument('--image', type=str,
@@ -34,16 +57,7 @@ if __name__ == "__main__":
     splitter = ImageSplitter(scale=args.scale)
 
     if args.image and args.output:
-        img = Image.open(args.image).convert('RGB')
-        patches = splitter.split(img)
-
-        start_time = time.time()
-        with torch.no_grad():
-            out = [model(p.to(device)) for p in patches]
-        print("Done in %.3f seconds!" % (time.time() - start_time))
-
-        out_img = splitter.merge(out)
-        out_img.save(args.output)
+        process(model, splitter, args.batch_size, args.image, args.output)
     elif args.input_dir and args.output_dir:
         if not os.path.exists(args.output_dir):
             os.makedirs(args.output_dir)
@@ -54,13 +68,4 @@ if __name__ == "__main__":
                     input_fname = os.path.join(root, name)
                     output_fname = os.path.join(args.output_dir, f'{os.path.splitext(name)[0]}_x{args.scale}.png')
 
-                    img = Image.open(input_fname).convert('RGB')
-                    patches = splitter.split(img)
-
-                    start_time = time.time()
-                    with torch.no_grad():
-                        out = [model(p.to(device)) for p in patches]
-                    print("Done in %.3f seconds!" % (time.time() - start_time))
-
-                    out_img = splitter.merge(out)
-                    out_img.save(output_fname)
+                    process(model, splitter, args.batch_size, input_fname, output_fname)
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import copy
+import math
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 
 
@@ -12,25 +14,36 @@ class ImageSplitter:
         self.pad_size = pad_size
         self.height = 0
         self.width = 0
-        self.ref_pad = nn.ReplicationPad2d(self.pad_size)
 
     def split(self, pil_img):
         img_tensor = TF.to_tensor(pil_img).unsqueeze_(0)
-        img_tensor = self.ref_pad(img_tensor)
+        _, _, h, w = img_tensor.size()
+
+        pad_h = self.seg_size * math.ceil(w/self.seg_size)
+        pad_v = self.seg_size * math.ceil(h/self.seg_size)
+        pad_h = pad_h - w
+        pad_v = pad_v - h
+
+        if pad_h % 2 == 0:
+            self.pad_l, self.pad_r = int(pad_h/2 + self.pad_size), int(pad_h/2 + self.pad_size)
+        else:
+            self.pad_l, self.pad_r = math.floor(pad_h/2) + self.pad_size, math.ceil(pad_h/2) + self.pad_size
+        if pad_v % 2 == 0:
+            self.pad_t, self.pad_b = int(pad_v/2 + self.pad_size), int(pad_v/2 + self.pad_size)
+        else:
+            self.pad_t, self.pad_b =  math.floor(pad_v/2) + self.pad_size, math.ceil(pad_v/2) + self.pad_size
+
+        img_tensor = F.pad(img_tensor, (self.pad_l, self.pad_r, self.pad_t, self.pad_b), 'reflect')
         _, _, h, w = img_tensor.size()
         self.height = h
         self.width = w
 
-        if h % self.seg_size < self.pad_size or w % self.seg_size < self.pad_size:
-            self.seg_size += self.scale * self.pad_size
-
         patches = []
-        for i in range(self.pad_size, h, self.seg_size):
-            for j in range(self.pad_size, w, self.seg_size):
+        for i in range(self.pad_size, h-self.pad_size, self.seg_size):
+            for j in range(self.pad_size, w-self.pad_size, self.seg_size):
                 patch = img_tensor[:, :,
                     (i-self.pad_size):min(i+self.pad_size+self.seg_size, h),
                     (j-self.pad_size):min(j+self.pad_size+self.seg_size, w)]
-
                 patches.append(patch)
 
         return patches
@@ -38,19 +51,24 @@ class ImageSplitter:
     def merge(self, patches):
         pad_size = self.scale * self.pad_size
         seg_size = self.scale * self.seg_size
+        pad_l = self.scale * self.pad_l
+        pad_r = self.scale * self.pad_r
+        pad_t = self.scale * self.pad_t
+        pad_b = self.scale * self.pad_b
         height = self.scale * self.height
         width = self.scale * self.width
 
         out = torch.zeros((1, 3, height, width))
         patch_tensors = copy.copy(patches)
 
-        for i in range(pad_size, height, seg_size):
-            for j in range(pad_size, width, seg_size):
+        for i in range(pad_size, height-pad_size, seg_size):
+            for j in range(pad_size, width-pad_size, seg_size):
                 patch = patch_tensors.pop(0)
                 patch = patch[:, :, pad_size:-pad_size, pad_size:-pad_size]
 
                 _, _, h, w = patch.size()
                 out[:, :, i:i+h, j:j+w] = patch
-        out = out[:, :, pad_size:-pad_size, pad_size:-pad_size]
+
+        out = out[:, :, pad_t:-pad_b, pad_l:-pad_r]
 
         return TF.to_pil_image(out.clamp_(0,1).squeeze_(0))
